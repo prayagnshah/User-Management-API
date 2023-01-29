e593c1ff26e9f5276208303731c2be46e236ea26
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2023-01-29 00:26:13 -0400

    updated the id function and id returns correctly

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 ##importing flask framework
 
-from flask import Flask, json, request, abort
+from flask import Flask, json, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
@@ -44,65 +44,15 @@ def write_users():
     return str(True)
 
 @app.route('/v1/users/<request_id>', methods=['GET'])
-def get_id(request_id):
+def get_employee(request_id):
     data = read_db()
+    # user_exist = False
 
     ##parsing the dictionary users from our json file
 
     for user in data['users']:
-    #using the if else condition to show that user exists.
+        if user["id"] == request_id:
+            # user_exist = True
+            return jsonify({'name': user['name']})
 
-        user_id = user['id']
-
-        if user_id == request_id:
-            return user
-
-    return f"ID" + "\n" + request_id + "\n" + "does not exist"
-    return user_id
-
-
-# @app.route('/v1/users/<id>', methods=['DELETE'])
-# def del_id(id):
-#     data = read_db()
-
-#     for user in data['users']:
-
-
-    # for users in enumerate(data):
-
-        # for alpha in users.items():
-        #     print(alpha)
-        #     if alpha == id_enter:
-        #         return users
-        #     else:
-        #         return "Error"
-
-        user_id = user["id"]
-
-        new_data = []
-
-        if user_id == user['id']:
-            data['users'].remove(user)
-
-            new_data.append(user)
-            return new_data
-        else:
-            return "Wrong ID"
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/v1/users/<id>')
-
-    ## .load
-# def write_db():
-
-#     ## .dump
+    return jsonify({'error': 'User with id does not exist'})
