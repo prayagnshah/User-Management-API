23dd908159b3fb0cee6cb2d0ad0e64d28c5d2174
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2023-01-30 00:08:34 -0400

    successfully able to add the new data in database
    - Added user named Danish in database

--- application.py
+++ application.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, json, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
 app = Flask(__name__)
 
 
@@ -36,24 +37,26 @@ def index():
 def read_users():
     return read_db()
 
-# Using this endpoint to add the new data into the database
-@app.route('/v1/users', methods = ['POST'])
-def write_users():
-    db = read_db()
-    users = db.get('users')
-    new_user = request.json  ##how to get the body in postman
-    users.append(new_user)  ##how to write new information in user
-    write_db(db)
-    return jsonify(str(True))
+# # Using this endpoint to add the new data into the database
+# @app.route('/v1/users', methods = ['POST'])
+# def write_users():
+#     db = read_db()
+#     users = db.get('users')
+#     new_user = request.json  ##how to get the body in postman
+#     users.append(new_user)  ##how to write new information in user
+#     write_db(db)
+#     return jsonify(str(True))
 
-#Using the endpoint to retrieve the data once the user enters the ID from database
+#Using the endpoint to retrieve the data
+# once the user enters the ID from database
 
 @app.route('/v1/users/<request_id>', methods=['GET'])
 def get_employee(request_id):
     data = read_db()
 
     #parsing the dictionary users from our json file
-    # using get function in order to save the system from throwing an error from the database
+    # using GET function in order to save the system from
+    # throwing an error from the database
 
     for user in data.get('users'):
         if user.get('id') == request_id:
@@ -68,7 +71,8 @@ def get_employee(request_id):
 def delete_id(request_id):
     data = read_db()
 
-    # index variable will store all json names and user will store the ID matcing the names
+    # index variable will store all json names and user will
+    # store the ID matcing the names
     for index, user in enumerate(data['users']):
         if user.get('id') == request_id:
             del data['users'][index]
@@ -79,3 +83,49 @@ def delete_id(request_id):
             return jsonify({'result': 'User with id {} deleted successfully'.format(request_id)})
 
     return jsonify({'error': 'User with {} does not exist'.format(request_id)}), 404
+
+# Using this endpoint to create the POST request to add the new user
+# into the database
+
+@app.route('/v1/users', methods=['POST'])
+
+def add_user():
+
+    # requesting input in json format
+    request_data = request.get_json()
+
+    # checking the condition if name, age and team are matching
+    if 'name' in request_data and 'age' in request_data and 'team' in request_data:
+        new_user = {
+            'id': str(uuid.uuid4()),
+            'name': request_data['name'],
+            'age': request_data['age'],
+            'team': request_data['team']
+        }
+        data = read_db()
+
+        # appending to the current database
+        data['users'].append(new_user)
+        write_db(data)
+
+        return jsonify({'message': 'User added successfully'})
+    else:
+        return jsonify({'error': 'Invalid data, name, age and team are required'}), 400
+
+
+    # if name in request_data and 'age' in request_data and 'team' in request_data:
+    #     new_user = ({ 'name' = request_data['name'], 'age' = request_data['age'], 'team' = request_data['team'] })
+    # # requesting data as per the question
+
+
+    # if not name or not age or not team:
+    #     return jsonify({'error': 'Cannot create user as following attributes are missing:'}), 400
+
+
+    # # if 'name' in request_data and 'age' in request_data and 'team' in request_data:
+
+    # #     new_user = {
+
+    # #     }
+
+
